7bafebc9265247cdd6317dfd416774e89be0f917
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-10-22 20:16:43 +0300

    Add `Storage` class docs

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,14 +1,45 @@
+/**
+ * Setings storage class.
+ * @class Storage.
+ */
 class Storage {
+  /**
+   * Settings key prefix.
+   * @type {string}
+   * @memberof Storage
+   */
   #prefix;
 
+  /**
+   * Creates an instance of Storage.
+   * @param {string} [prefix=""] Prefix for settings keys.
+   * @memberof Storage
+   */
   constructor(prefix = "") {
     this.#prefix = prefix;
   }
 
+  /**
+   * Check if selected storage area is available.
+   *
+   * @param {string} area Storage area (local, sync, managed).
+   * @returns {boolean}
+   * @link https://developer.chrome.com/docs/extensions/reference/storage/#properties
+   * @memberof Storage
+   */
   check(area) {
     return typeof chrome.storage[area] !== "undefined";
   }
 
+  /**
+   * Create or overwrite setting key.
+   *
+   * @param {string} area Storage area (local, sync, managed).
+   * @param {string} key Setting key.
+   * @param {any} value Setting value.
+   * @returns {Promise<void>}
+   * @memberof Storage
+   */
   set(area, key, value) {
     return new Promise((resolve, reject) => {
       if (!this.check(area)) {
@@ -20,6 +51,14 @@ class Storage {
     });
   }
 
+  /**
+   * Get value of a setting key.
+   *
+   * @param {string} area Storage area (local, sync, managed).
+   * @param {string} key Setting key.
+   * @returns {Promise<any|null>}
+   * @memberof Storage
+   */
   get(area, key) {
     return new Promise((resolve, reject) => {
       if (!this.check(area)) {
@@ -33,6 +72,14 @@ class Storage {
     });
   }
 
+  /**
+   * Delete a setting key.
+   *
+   * @param {string} area Storage area (local, sync, managed).
+   * @param {string} key Setting key.
+   * @returns {Promise<void>}
+   * @memberof Storage
+   */
   del(area, key) {
     return new Promise((resolve, reject) => {
       if (!this.check(area)) {
